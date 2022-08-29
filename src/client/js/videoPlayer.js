e7b9ea0f13679009c9f66875500c0ef4659474a3
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-08-29 23:14:09 +0900

    controls event styles 적용
    timeout 이용해 마우스 커서 움직임 멈출 경우 정해진 시간 이후 video controls 숨기기, 스크롤 바 style 적용, 비디오 controls style 적용

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,9 +4,10 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
-const timeline = document.getElementById("timeline");
+const timeline = document.getElementById("timeline"); // range type의 input으로, 비디오의 현재 재생 정도 나타냄
 const fullScreenBtn = document.getElementById("fullScreen");
 const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
 let volumeValue = 0.5; // 볼륨 기본 값으로 0.5 설정, 이때 volumeValue는 global 변수
 video.volume = volumeValue; // video.volume에 기본값 적용
@@ -20,7 +21,9 @@ const handlePlayClick = () => {
     video.pause();
   }
 
-  playBtn.innerText = video.paused ? "Play" : "Pause";
+  playBtn.childNodes[0].className = video.paused
+    ? "fa-solid fa-play"
+    : "fa-solid fa-pause";
 };
 
 const handleMuteClick = () => {
@@ -30,7 +33,9 @@ const handleMuteClick = () => {
     video.muted = true;
   }
 
-  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  muteBtn.childNodes[0].className = video.muted
+    ? "fa-solid fa-volume-xmark"
+    : "fa-solid fa-volume-high";
   volume.value = video.muted ? 0 : volumeValue; // mute가 해제될 때 0.5가 아닌 volumeValue로 비디오 볼륨 설정
 };
 
@@ -39,36 +44,39 @@ const handleVolumeChange = (event) => {
   const {
     target: { value },
   } = event;
-
-  if (video.volume > 0) {
-    video.muted = false;
-    muteBtn.innerText = "Mute";
-  }
-  // else {
-  //   video.muted = true;
-  //   muteBtn.innerText = "Unmute";
-  // }
-
   volumeValue = value; // 볼륨 range 바뀔때마다 volumeValue도 그 값으로 변경
   video.volume = value;
+
+  // 아래 코드에서 아이콘 바꾸기 이전에
+  // 위에서 video.volume과 v
+  if (video.volume > 0) {
+    video.muted = false;
+    muteBtn.childNodes[0].className = "fa-solid fa-volume-high";
+  } else {
+    video.muted = true;
+    muteBtn.childNodes[0].className = "fa-solid fa-volume-xmark";
+  }
 };
 
 // Date() 함수를 이용해 비디오의 currentTime과 totalTime의 포맷을 "00:00"으로 바꿔주는 함수
 const formatTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substr(14, 5);
 
+// video의 metadata인 totalTime 등의 값이 로드됐을 때 발생하는 이벤트로
+// 해당 video의 duration 값 통해 totalTime의 innerText와 timeline.max 설정하는 함수
 const handleLoadedMetaData = () => {
+  // console.log(Math.floor(video.duration));
   // console.log(event);
   // totalTime.innerText = Math.floor(video.duration); // 비디오의 전체 길이
-  totalTime.innerText = formatTime(Math.floor(video.duration));
+  totalTime.innerText = formatTime(Math.floor(video.duration)); // 비디오의 전체 길이 설정
 
-  // timeline의 maximum value 설정해주기
-  timeline.max = Math.floor(video.duration);
+  timeline.max = Math.floor(video.duration); // timeline의 maximum value 설정
 };
 
+// timeupdate event 통해 시간이 업데이트될 때마다 currentTime과 timeline 값 설정해주는 함수
 const handleTimeUpdate = () => {
   currentTime.innerText = formatTime(Math.floor(video.currentTime)); // 비디오의 현재 재생 시간
-  timeline.value = Math.floor(video.currentTime);
+  timeline.value = Math.floor(video.currentTime); // 비디오 재생 과정(timeline)
 };
 
 // timeline의 value값이 변할 때마다 비디오 시간도 변경되도록 하는 함수
@@ -83,12 +91,43 @@ const handleFullScreen = () => {
   // 풀스크린 상태가 아닌 경우
   if (!fullscreen) {
     videoContainer.requestFullscreen(); // 주의할 점! video 요소말고, 모든 기본 버튼까지 포함된 videoContainer에 적용해줘야 함
-    fullScreenBtn.innerText = "Exit Full Screen";
+    fullScreenBtn.childNodes[0].className = "fa-solid fa-compress";
   } else {
     // 풀스크린 상태인 경우
     document.exitFullscreen(); // 풀 스크린 모드 해제
-    fullScreenBtn.innerText = "Enter Full Screen";
+    fullScreenBtn.childNodes[0].className = "fa-solid fa-expand";
   }
+};
+
+let controlsTimeout = null;
+let controlsMovementTimeout = null;
+
+const hideControls = () => videoControls.classList.remove("show");
+
+const handleMouseMove = () => {
+  if (controlsTimeout) {
+    // mouseleave 후 timeout 완료되지 않았을 때 다시 마우스 돌아왔다면
+    // timeout 취소 (clearrTimeout) 및 null로 초기화
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+  if (controlsMovementTimeout) {
+    // controlsMovementTimeout이 null이지 않을 때
+    // 즉 마우스가 계속해서 움직이고 있다면?! 이 if문이 실행돼서
+    // setTimeout으로 인해 3초 뒤 controls가 없어지지 않도록 clearTimeout 실행됨
+    // 반면, 마우스가 움직임을 멈춘다면 이 if문은 실행되지 않음
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
+  }
+  videoControls.classList.add("show");
+  controlsMovementTimeout = setTimeout(hideControls, 2000); // 마우스 움직일 시 setTimeout 재설정
+};
+
+const handleMouseLeave = () => {
+  controlsTimeout = setTimeout(hideControls, 2000);
+  // mouseleave 시 2초 후 show class remove
+  // console.log(controlsTimeout);
+  // clearTimeout(id); // timeout이 취소됨
 };
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -98,3 +137,5 @@ video.addEventListener("loadedmetadata", handleLoadedMetaData);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullScreen);
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
