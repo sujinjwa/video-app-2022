c4c0677271f581bdf3273e07b693c937ce531ded
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-08 22:16:38 +0900

    style: 문자열에 작은 따옴표 사용하도록 수정

--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
-import express from "express";
+import express from 'express';
 // import multer from "multer";
-import morgan from "morgan";
-import session from "express-session";
-import flash from "express-flash";
-import MongoStore from "connect-mongo";
-import rootRouter from "./routers/rootRouter";
-import videoRouter from "./routers/videoRouter";
-import userRouter from "./routers/userRouter";
-import apiRouter from "./routers/apiRouter";
-import { localsMiddleware } from "./middlewares";
+import morgan from 'morgan';
+import session from 'express-session';
+import flash from 'express-flash';
+import MongoStore from 'connect-mongo';
+import rootRouter from './routers/rootRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
+import apiRouter from './routers/apiRouter';
+import { localsMiddleware } from './middlewares';
 
 const app = express();
 // const multer = require("multer");
-const logger = morgan("dev");
+const logger = morgan('dev');
 
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views"); // process.cwd() = wetube-clone-2022 폴더
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views'); // process.cwd() = wetube-clone-2022 폴더
 app.use(logger);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // 서버에 데이터 전송하는 form 기능 가능하게 하는 미들웨어
+app.use(express.json()); // 문자열을 js object 로 변경시켜주는 미들웨어
 
 // console.log(process.env.COOKIE_SECRET);
 // router 앞에서 session 미들웨어 추가
@@ -54,11 +54,11 @@ app.use(
 
 app.use(flash());
 app.use(localsMiddleware);
-app.use("/uploads", express.static("uploads"));
-app.use("/assets", express.static("assets"));
-app.use("/", rootRouter);
-app.use("/videos", videoRouter);
-app.use("/users", userRouter);
-app.use("/api", apiRouter);
+app.use('/uploads', express.static('uploads'));
+app.use('/assets', express.static('assets'));
+app.use('/', rootRouter);
+app.use('/videos', videoRouter);
+app.use('/users', userRouter);
+app.use('/api', apiRouter);
 
 export default app;
